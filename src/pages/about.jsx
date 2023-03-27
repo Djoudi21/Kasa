1f8d19c9cbb13411d5b6b7a133628637db11a35a
Djoudi21 <abdelkrim@gdpr.dev>
2023-03-27 22:59:24 +0200

    add about component

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,7 +1,11 @@
+import Layout from "../components/Layout";
+
 export default function About() {
     return (
         <>
+            <Layout>
             <div>About</div>
+            </Layout>
         </>
     );
 }
