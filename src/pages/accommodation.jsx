4cda6a7fd94ff962a1ba504bc17e265562bc7de5
Djoudi21 <abdelkrim@gdpr.dev>
2023-03-27 22:58:48 +0200

    add accommodation component

---
+++ src/pages/accommodation.jsx
@@ -0,0 +1,11 @@
+import Layout from "../components/Layout";
+
+export default function Accommodation() {
+    return (
+        <>
+            <Layout>
+                <div>Accommodation</div>
+            </Layout>
+        </>
+    );
+}
