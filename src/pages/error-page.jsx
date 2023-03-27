1947fa542fac387eb3f856961c33f1c8df26c58c
Djoudi21 <abdelkrim@gdpr.dev>
2023-03-28 01:17:59 +0200

    add and style 404

--- src/pages/error-page.jsx
+++ src/pages/error-page.jsx
@@ -1,16 +1,22 @@
-import { useRouteError } from "react-router-dom";
+import {Link, useRouteError} from "react-router-dom";
+import Layout404 from "../components/Layout404";
+import styles from '../css/_ErrorPage.module.css';
+
 
 export default function ErrorPage() {
     const error = useRouteError();
     console.error(error);
 
     return (
-        <div id="error-page">
-            <h1>Oops!</h1>
-            <p>Sorry, an unexpected error has occurred.</p>
-            <p>
-                <i>{error.statusText || error.message}</i>
-            </p>
-        </div>
+        <Layout404>
+            <div className={styles.container}>
+                <h1 className={styles.title}>404</h1>
+                <p className={styles.text}>
+                    <i>Oups! La page que
+                        vous demandez n'existe pas.</i>
+                </p>
+                <Link className={styles.link} to={'/'}>Retourner sur la page d’accueil</Link>
+            </div>
+        </Layout404>
     );
 }
