b566a67071e796e743addc8371f829c61632b3cf
Djoudi21 <abdelkrim@gdpr.dev>
2023-03-29 03:17:25 +0200

    add context and useEffect to fecth accommodations

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,14 +1,20 @@
 import Banner from "../components/Banner";
 import CardList from "../components/CardsList";
+import {useContext, useEffect} from "react";
+import {AccommodationsContext} from "../utils";
 
 export default function Home() {
+    const {accommodations, FetchAccommodations} = useContext(AccommodationsContext)
+    useEffect(() => {
+        FetchAccommodations()
+    }, [FetchAccommodations]);
     return (
         <>
             <section>
                 <Banner size={"small"} />
             </section>
             <section>
-                <CardList />
+                <CardList accommodations={accommodations} />
             </section>
         </>
     );
