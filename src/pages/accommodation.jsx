b1c9505cf88280f99de93a2a49a4046d8a392416
Djoudi21 <abdelkrim@gdpr.dev>
2023-03-29 03:17:37 +0200

    wip

--- src/pages/accommodation.jsx
+++ src/pages/accommodation.jsx
@@ -1,42 +1,29 @@
 import Layout from "../components/Layout";
 import styles from "../css/_Accommodation.module.css";
 import Banner from "../components/Banner";
-import Collapse from "../components/Collapse";
-import Tag from "../components/Tag";
+// import Collapse from "../components/Collapse";
+// import Tag from "../components/Tag";
+import {useContext, useEffect, useState} from "react";
+import {AccommodationsContext} from "../utils";
+import {useParams} from "react-router-dom";
+
 
 export default function Accommodation() {
-    const accommodation =   {
-        "id": "c67ab8a7",
-        "title": "Appartement cosy",
-        "cover": "https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-20-1.jpg",
-        "pictures": [
-            "https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-20-1.jpg",
-            "https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-20-2.jpg",
-            "https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-20-3.jpg",
-            "https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-20-4.jpg",
-            "https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-20-5.jpg"
-        ],
-        "description": "Votre maison loin de chez vous. Que vous veniez de l'autre bout du monde, ou juste de quelques stations de RER, vous vous sentirez chez vous dans notre appartement.",
-        "host": {
-            "name": "Nathalie Jean",
-            "picture": "https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/profile-picture-12.jpg"
-        },
-        "rating": "5",
-        "location": "Ile de France - Paris 17e",
-        "equipments": [
-            "Équipements de base",
-            "Micro-Ondes",
-            "Douche italienne",
-            "Frigo",
-            "WIFI"
-        ],
-        "tags": [
-            "Batignolle",
-            "Montmartre"
-        ]
-    }
+    const idFromParams = useParams()
+    const [accommodation, setAccommodation] = useState({})
 
-        return (
+    const {accommodations, FetchAccommodations} = useContext(AccommodationsContext)
+    useEffect(() => {
+        FetchAccommodations()
+        const filteredAccommodations = filterAccommodation()
+        setAccommodation(filteredAccommodations[0])
+    }, [FetchAccommodations]);
+    function filterAccommodation() {
+       return accommodations.filter(el => el.id === idFromParams.id)
+    }
+    console.log(accommodation)
+
+    return (
         <>
             <Layout>
                 <div className={styles.container}>
@@ -45,12 +32,12 @@ export default function Accommodation() {
                     </section>
 
                     <section>
-                        <h3 className={styles.title}>{accommodation.title}</h3>
-                        <span className={styles.location}>{accommodation.location}</span>
+                        {/*<h3 className={styles.title}>{accommodation &&accommodation.title}</h3>*/}
+                        {/*<span className={styles.location}>{accommodation && accommodation.location}</span>*/}
                         <div className={styles.tags}>
-                            {accommodation.tags.map((el) => (
-                                <Tag title={el} />
-                            ))}
+                            {/*{accommodation.tags && accommodation.tags.map((el) => (*/}
+                            {/*    <Tag title={el} />*/}
+                            {/*))}*/}
                         </div>
                         //TODO: handle rating function
                         <div className={styles.rating}>
@@ -70,11 +57,10 @@ export default function Accommodation() {
                                 <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M9.8225 6.5L8 0.5L6.1775 6.5H0.5L5.135 9.8075L3.3725 15.5L8 11.9825L12.635 15.5L10.8725 9.8075L15.5 6.5H9.8225Z" fill="#FF6060"/>
                                 </svg>
-
                             </div>
                             <div className={styles.flex}>
                                 <div className={styles.tutu}>
-                                    <span>{accommodation.host.name}</span>
+                                    {/*<span>{accommodation && accommodation.host.name}</span>*/}
                                 </div>
                                 //TODO: handle avatar pic
                                 <svg width="33" height="33" viewBox="0 0 33 33" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -84,14 +70,13 @@ export default function Accommodation() {
                         </div>
                     </section>
                     <section>
-                        <Collapse title={"Description"} text={accommodation.description} />
+                        {/*<Collapse title={"Description"} text={accommodation && accommodation.description} />*/}
                     </section>
                     <section>
-                        <Collapse title={"Equipment"} list={accommodation.equipments} />
+                        {/*<Collapse title={"Equipment"} list={accommodation && accommodation.equipments} />*/}
                     </section>
                 </div>
             </Layout>
-
         </>
     );
 }
