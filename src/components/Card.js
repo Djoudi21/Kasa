ef62bd1827f126249c72a3045042046230d4276f
Djoudi21 <abdelkrim@gdpr.dev>
2023-03-29 03:16:14 +0200

    add image to card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,10 @@
 import styles from '../css/_Cards.module.css';
 
-export default function Card({title}) {
+export default function Card({title, cover}) {
     return (
         <>
             <div className={styles.container}>
+                <img className={styles.img} src={cover} alt=""/>
                 <h3 className={styles.title}>{title}</h3>
             </div>
         </>
