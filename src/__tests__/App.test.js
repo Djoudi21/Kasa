1bfa5d898d3a15b30eefbcd45561859805b7a49d
Djoudi21 <abdelkrim@gdpr.dev>
2023-03-30 19:59:24 +0200

    Updates

--- src/__tests__/App.test.js
+++
@@ -1,8 +0,0 @@
-// import { render, screen } from '@testing-library/react';
-// import App from '../components/App';
-// import Home from '../pages/Home';
-// import {AccommodationsProvider} from "../utils";
-
-test('renders learn react link', () => {
-  expect(true).toBe(true);
-});
