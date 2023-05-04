62e33aac61a6db168554fcf2f75f472674b99437
Djoudi21 <abdelkrim@gdpr.dev>
2023-05-04 20:51:02 +0200

    commit

--- src/components/banners/AboutBanner.jsx
+++ src/components/banners/AboutBanner.jsx
@@ -1,9 +1,13 @@
 import styles from '../../css/_Banner.module.css';
+import logo from './titi.jpg';
+
 
 export default function AboutBanner() {
     return (
         <>
-            <div className={`${styles.container} ${styles.aboutBackGroundImg} ${styles.cardHeightAbout}  ${styles.cardHeightAbout}`}/>
+            <div className={`${styles.container} ${styles.w90} ${styles.cardHeightAbout}  ${styles.cardHeightAbout}`}>
+                <img className={`${styles.aboutBackGroundImg}`} src={logo}  alt={'toto'}/>
+            </div>
         </>
     );
 }
--- src/components/banners/HomeBanner.jsx
+++ src/components/banners/HomeBanner.jsx
@@ -1,10 +1,12 @@
 import styles from '../../css/_Banner.module.css';
+import logo from "../../assets/img-banner.png";
 
 export default function HomeBanner() {
     return (
         <>
-            <div className={`${styles.container} ${styles.homeBackGroundImg} ${styles.cardHeightHome} ${styles.cardHeightDesktop}`}>
-                <div className={styles.flex}>
+            <div className={`${styles.container} ${styles.w75} ${styles.p50} ${styles.homeBackGroundImg} ${styles.cardHeightHome} ${styles.cardHeightDesktop}`}>
+                <img className={`${styles.homeBackGroundImg}`} src={logo}  alt={'toto'}/>
+                <div className={`${styles.flex} ${styles.textContainer}`}>
                     <h3 className={styles.title}>Chez vous,</h3>
                     <h3 className={styles.title}>partout et ailleurs</h3>
                 </div>
